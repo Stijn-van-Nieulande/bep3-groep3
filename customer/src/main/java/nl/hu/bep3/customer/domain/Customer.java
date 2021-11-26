fb6453a83cb6d92b56eb65ddc88c94673024a274
Erik <erik.meijer@student.hu.nl>
2021-11-26 13:53:21 +0100

    removed review

--- customer/src/main/java/nl/hu/bep3/customer/domain/Customer.java
+++ customer/src/main/java/nl/hu/bep3/customer/domain/Customer.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 public class Customer {
     private CustomerId id;
-    private List<ReviewId> reviewIds;
-
+    private String address;
+    private String email;
+    private String phoneNumber;
 }
--- customer/src/main/java/nl/hu/bep3/customer/domain/Review.java
+++
@@ -1,6 +0,0 @@
-package nl.hu.bep3.customer.domain;
-
-public class Review {
-    private int rating;
-    private String review;
-}
