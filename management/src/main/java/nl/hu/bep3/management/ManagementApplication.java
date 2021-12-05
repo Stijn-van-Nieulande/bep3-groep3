1755f105cff1886624842d5989e58fa3b1e8a8e3
Stijn van Nieulande <stijn.van.nieulande@gmail.com>
2021-12-05 22:59:36 +0100

    just some untested random junk

--- management/src/main/java/nl/hu/bep3/management/ManagementApplication.java
+++ management/src/main/java/nl/hu/bep3/management/ManagementApplication.java
@@ -2,21 +2,12 @@ package nl.hu.bep3.management;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication(scanBasePackages = "nl.hu.bep3.management")
-@RestController
 public class ManagementApplication
 {
     public static void main(final String[] args)
     {
         SpringApplication.run(ManagementApplication.class, args);
-    }
-
-    @GetMapping("/")
-    public String home()
-    {
-        return "Hello from management";
     }
 }
---
+++ management/src/main/java/nl/hu/bep3/management/domain/Owner.java
@@ -0,0 +1,16 @@
+package nl.hu.bep3.management.domain;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document
+public class Owner
+{
+    @Id
+    private Long id;
+
+    private String firstName;
+    private String lastName;
+    private Double salaris;
+    private Role role;
+}
