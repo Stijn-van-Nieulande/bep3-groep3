a918ddc2d338cbb8038d21fd5e903a5000c6069c
Stijn van Nieulande <stijn.van.nieulande@gmail.com>
2021-12-20 23:26:55 +0100

    changed service to configuration

--- lib-swagger-data-provider/src/main/java/nl/hu/bep3/libswaggerdataprovider/SwaggerDataProviderConfig.java
+++ lib-swagger-data-provider/src/main/java/nl/hu/bep3/libswaggerdataprovider/SwaggerDataProviderConfig.java
@@ -2,11 +2,11 @@ package nl.hu.bep3.libswaggerdataprovider;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
-import org.springframework.stereotype.Service;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@Service
+@Configuration
 public class SwaggerDataProviderConfig {
   @Value("${app.swagger.endpoint}")
   private String swaggerEndpoint;
