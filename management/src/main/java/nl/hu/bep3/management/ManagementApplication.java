58f4bba612e5fad1a419bc83bd712e3022d2c30a
Stijn van Nieulande <stijn.van.nieulande@gmail.com>
2021-12-20 23:20:18 +0100

    deleted own cors

--- management/src/main/java/nl/hu/bep3/management/ManagementApplication.java
+++ management/src/main/java/nl/hu/bep3/management/ManagementApplication.java
@@ -3,21 +3,15 @@ package nl.hu.bep3.management;
 import nl.hu.bep3.management.domain.Employee;
 import nl.hu.bep3.management.domain.Role;
 import nl.hu.bep3.management.domain.service.EmployeeService;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication(scanBasePackages = "nl.hu.bep3.management")
 @EnableFeignClients("nl.hu.bep3.management.proxy")
 public class ManagementApplication {
-  @Value("${app.swagger.endpoint}")
-  private String swaggerEndpoint;
-
   public static void main(final String[] args) {
     SpringApplication.run(ManagementApplication.class, args);
   }
@@ -34,19 +28,6 @@ public class ManagementApplication {
                   Role.THE_PERSON_BEHIND_THE_DESK_THAT_ACTUALLY_DOESNT_WORK_HERE));
 
       System.out.println(employee.getId());
-    };
-  }
-
-  @Bean
-  public WebMvcConfigurer corsConfigurer() {
-    System.out.println(ManagementApplication.this.swaggerEndpoint);
-    return new WebMvcConfigurer() {
-      @Override
-      public void addCorsMappings(final CorsRegistry registry) {
-        registry
-            .addMapping("/v3/api-docs")
-            .allowedOrigins(ManagementApplication.this.swaggerEndpoint);
-      }
     };
   }
 }
