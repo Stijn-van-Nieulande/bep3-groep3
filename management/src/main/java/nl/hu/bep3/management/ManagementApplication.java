dd4daf67fb037c128bd4caeb26935b02710fcc80
Stijn van Nieulande <stijn.van.nieulande@gmail.com>
2021-12-21 02:29:25 +0100

    added swagger

--- management/src/main/java/nl/hu/bep3/management/ManagementApplication.java
+++ management/src/main/java/nl/hu/bep3/management/ManagementApplication.java
@@ -1,5 +1,6 @@
 package nl.hu.bep3.management;
 
+import nl.hu.bep3.libswaggerdataprovider.SwaggerDataProviderConfig;
 import nl.hu.bep3.management.domain.Employee;
 import nl.hu.bep3.management.domain.Role;
 import nl.hu.bep3.management.domain.service.EmployeeService;
@@ -8,9 +9,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication(scanBasePackages = "nl.hu.bep3.management")
 @EnableFeignClients("nl.hu.bep3.management.proxy")
+@Import(SwaggerDataProviderConfig.class)
 public class ManagementApplication {
   public static void main(final String[] args) {
     SpringApplication.run(ManagementApplication.class, args);
